6cf8d98ffb596ce4645ee25b19f97f7e349f7939
juho0428@naver.com <juho0428@naver.com>
2021-03-11 16:20:48 +0900

    first commit

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,3 +1,16 @@
+import Head from 'next/head'
+import Layout, { siteTitle } from '../../components/layout'
+import utilStyles from '../../styles/utils.module.css'
+
 export default function FirstPost() {
-    return <h1>First Post !</h1>
-}
+    return (
+    <Layout firstpost>
+        <Head>
+          <title>{siteTitle}</title>
+        </Head>
+        <section className={utilStyles.headingMd}>
+            <h1>First Post !</h1>
+        </section>
+      </Layout>
+    )
+} 
